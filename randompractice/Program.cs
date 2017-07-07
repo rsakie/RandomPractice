7409d9644cf98600dd241d8956e15d54606662d6
rsakie <rsakie@yahoo.com>
2017-07-07 12:04:09 -0400

    inital random practice

---
+++ randompractice/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace randompractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////create random object
+            //Random randomNumber = new Random();
+
+
+            //example 1
+            ////saving the random number from our random object(randomNumber)
+            //int randy = randomNumber.Next(0, 2);
+            ////printing out the random int variable
+            ////once int a variable it saves and can use anytime through code
+            //Console.WriteLine(randy);
+            //Console.WriteLine(randy);
+            //Console.WriteLine(randy);
+            ////did not save that value to an integer
+            //Console.WriteLine(randomNumber.Next(0,9));
+            //Console.WriteLine(randy);
+
+            //example 2
+            //string [] words = { "Paper", "Rock", "Scissors" };
+            //Console.WriteLine(words[randomNumber.Next(0,3)]);
+
+            //example 3
+            //string[] words = { "Paper", "Rock", "Scissors" };
+            //int[] numbers = { 1, 2, 3, 4, 76, 6 };
+            //Console.WriteLine(words[randomNumber.Next(0, 3)]);
+            //Console.WriteLine(numbers[randomNumber.Next(0, 5)]);
+
+
+            //example 4
+            //string[] words = { "Paper", "Rock", "Scissors" };
+            //int[] numbers = { 1, 2, 3, 4, 76, 6 };
+            //string sayings = "You can never have too much cake";
+            //Console.WriteLine(words[randomNumber.Next(0, 3)]);
+            //Console.WriteLine(numbers[randomNumber.Next(0, 5)]);
+            //Console.WriteLine(sayings[randomNumber.Next(0,sayings.Length)]);
+
+
+
+        }
+    }
+}
